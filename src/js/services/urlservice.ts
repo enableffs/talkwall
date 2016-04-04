160e6ee82f12be7cb2d7666c57f9d92b59b5b8e2
Jeremy Toussaint <jt151077@gmail.com>
2016-04-04 18:20:04 +0200

    - typescript conversion landing component - language autodetection based on domain extension - styling

--- src/js/services/urlservice.ts
+++ src/js/services/urlservice.ts
@@ -2,13 +2,25 @@
 
 module SamtavlaApp {
     "use strict";
+    import ILocationService = angular.ILocationService;
 
     export interface IURLService {
+        getDomain(): string;
     }
 
     export class URLService implements IURLService {
-        constructor() {
-            console.log('--> URLService started ...');
+        static $inject = ["$location"];
+        private domain: string  = 'en';
+        constructor( private $location: ILocationService ) {
+            if (this.$location.host().indexOf('.no') > -1) {
+                this.domain = 'no';
+            }
+
+            console.log('--> URLService started ... the locale used is: ' + this.domain);
+        }
+
+        getDomain(): string {
+            return this.domain;
         }
     }
 }
