919a84e239abd6d81564792829ec1ad3ea6e5a4d
Jeremy Toussaint <jt151077@gmail.com>
2016-06-01 11:29:15 +0200

    - mock wall - mock question + add new questions - sass based styling everywhere - right-hand side panel CSS (flexbox) - add Task panel layout

--- src/js/components/wall/wall.ts
+++ src/js/components/wall/wall.ts
@@ -17,25 +17,58 @@ module TalkwallApp {
 		 * Pop up bottom sheet to edit messages. Slide out left sidenav also
 		 */
 		showMessageEditor(): void;
+		/**
+		 * Toggles which right menu should be open
+		 */
+		toggleRightMenu(index: number): void;
+		/**
+		 * Post a new question
+		 */
+		postNewQuestion(): void;
 	}
 
 	export class WallController implements IWallControllerService {
 		static $inject = ['DataService', '$mdSidenav', '$mdBottomSheet'];
 		private magnified: boolean = false;
-		private question: Question;
+		private rightMenu1: boolean = false;
+		private rightMenu2: boolean = false;
+		private rightMenu3: boolean = false;
+		private question: Question = null;
+		private currentQuestionIndex: number = 0;
+		private newQuestion: string = '';
 		constructor(
 			private dataService: DataService,
 			private $mdSidenav: ISidenavService,
 			private $mdBottomSheet: IBottomSheetService) {
 			console.log('--> WallController: started: ');
 
-			if (this.dataService.checkAuthenticated()) {
-				this.activate();
-			}
+			var handle = this;
+			this.dataService.checkAuthenticated(function() {
+				handle.activate();
+			});
 		}
 
 		activate(): void {
 			console.log('--> WallController: activated');
+			//retrieve the first question of the current wall
+			if (this.dataService.getWall().questions.length > 0 && this.dataService.getWall().questions.length >= this.currentQuestionIndex) {
+				this.refreshQuestion();
+			} else {
+				this.rightMenu2 = true;
+				this.$mdSidenav('right').open();
+			}
+		}
+
+		refreshQuestion(): void {
+			var handle = this;
+			let idKey = '_id';
+			this.dataService.getQuestion(this.dataService.getWall().questions[this.currentQuestionIndex][idKey],
+				function(success: Question) {
+					handle.question = success;
+				},
+				function(error: {}) {
+					//TODO: handle question retrieval error
+				});
 		}
 
 		showMessageEditor(): void {
@@ -51,6 +84,45 @@ module TalkwallApp {
 				//dialog dismissed
 				console.log('--> WallController: dismissed');
 			});
+		}
+
+		toggleRightMenu(n: number): void {
+			console.log('--> WallController: toggleRightMenu: ' + n);
+			switch (n) {
+				case 1:
+					this.rightMenu1 = !this.rightMenu1;
+					this.rightMenu2 = false;
+					this.rightMenu3 = false;
+					break;
+				case 2:
+					this.rightMenu1 = false;
+					this.rightMenu2 = !this.rightMenu2;
+					this.rightMenu3 = false;
+					break;
+				case 3:
+					this.rightMenu1 = false;
+					this.rightMenu2 = false;
+					this.rightMenu3 = !this.rightMenu3;
+					break;
+				default:
+					this.rightMenu1 = false;
+					this.rightMenu2 = false;
+					this.rightMenu3 = false;
+			}
+		}
+
+		postNewQuestion(): void {
+			var handle = this;
+			this.dataService.addQuestion(this.newQuestion,
+				function() {
+					//success
+					handle.newQuestion = '';
+					handle.refreshQuestion();
+				},
+				function(error: {}) {
+					//TODO: handle question retrieval error
+				}
+			);
 		}
 	}
 }
